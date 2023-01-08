efca2a9ca79a5c27ea97ce9863449a9cda814984
maralt2001 <markus.altendorfer@kabelmail.de>
2023-01-08 12:24:43 +0100

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/maralt2001/pgcommonmodel
+
+go 1.19
+
+require github.com/satori/go.uuid v1.2.0
---
+++ main.go
@@ -0,0 +1,24 @@
+package pgcommonmodel
+
+import (
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+type User struct {
+	ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	CreatedAt time.Time
+	Email     string  `gorm:"email; not null;type:varchar(100);unique:true"`
+	Roles     []*Role `gorm:"many2many:user_roles;constraint:OnDelete:CASCADE"`
+}
+
+type Role struct {
+	ID    uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4()"`
+	Name  string    `gorm:"type:varchar(100);primaryKey;;constraint:OnDelete:CASCADE"`
+	Users []*User   `gorm:"many2many:user_roles;constraint:OnDelete:CASCADE"`
+}
+
+func (u *User) GetRolesCount() int {
+	return len(u.Roles)
+}
